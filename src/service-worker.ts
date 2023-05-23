d1dc90cdf60a13dd47f07fd6c62022b62c1de547
Arwer234 <filipowski.kuba@gmail.com>
2023-05-23 19:42:21 +0200

    fix(service-worker): fix self type

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -10,11 +10,27 @@
 
 import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
-import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
+import { precacheAndRoute, createHandlerBoundToURL, PrecacheEntry } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
 
-declare const self: ServiceWorkerGlobalScope;
+// TODO: Change back into ServiceWorkerGlobalScope
+declare const self: {
+  __WB_MANIFEST: (string | PrecacheEntry)[];
+  location: { origin: string };
+  addEventListener: (
+    arg0: string,
+    arg1: (
+      event: // See https://developers.google.com/web/tools/workbox/modules
+      // for the list of available Workbox modules, or add any other
+      // code you'd like.
+      // You can also remove this file if you'd prefer not to use a
+      // service worker, and the Workbox build step will be skipped.
+      any
+    ) => void
+  ) => void;
+  skipWaiting: () => void;
+};
 
 clientsClaim();
 
